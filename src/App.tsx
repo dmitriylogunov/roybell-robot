c7f4e4f5255b0a787db3fbc90640ba5c5e7d5c81
Dmitriy Logunov <logunovdm@gmail.com>
2024-08-18 22:19:00 +1000

    Update styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import GridComponent from "./components/Grid/GridComponent";
 function App() {
   return (
     <>
+      <div className="header"></div>
       <div className="content">
         <GridComponent />
       </div>
+      <div className="legend"></div>
+      <div className="footer"></div>
     </>
   );
 }
